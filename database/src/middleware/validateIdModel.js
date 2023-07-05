42ee6d3a1fd2994fb718d476414c1b2068869f6d
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-07-05 19:16:36 -0300

    manejador de error ID not found

--- database/src/middleware/validateIdModel.js
+++ database/src/middleware/validateIdModel.js
@@ -1,16 +1,21 @@
 const { ClientError } = require("../utils/errors");
+const store = require("../database");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const { model, id } = req.params;
 
   if (["Character", "Film", "Planet"].includes(model)) {
-    // Realiza la validación del ID para todos los modelos
     if (!Number.isInteger(parseInt(id))) {
-      throw new ClientError("Invalid ID", 404);
+      return next(new ClientError("Invalid ID", 404));
+    }
+
+    const document = await store[model].findById(id);
+    if (!document) {
+      return next(new ClientError("ID not found", 404));
     }
 
     return next();
   } else {
-    throw new ClientError("Invalid Model", 401);
+    return next(new ClientError("Invalid Model", 401));
   }
 };
