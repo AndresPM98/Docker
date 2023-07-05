2222ac3864c7a0ea5204d8649fcc4905b951b4f0
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-07-05 10:58:36 -0300

    controllers database

--- database/src/middleware/validateModel.js
+++ database/src/middleware/validateModel.js
@@ -1,8 +1,10 @@
-module.exports=(req,res,next)=>{
+const { ClientError } = require("../utils/errors")
+
+module.exports = (req,res,next)=>{
     const { model } = req.params;
-    if(["Character","Film", "Planet"].includes(model)) {
+    if(["Character", "Film", "Planet"].includes(model)) {
         return next();
     }else{
-        throw Error("Invalid Model")
+        throw new ClientError ("Invalid Model", 401)
     };
 };
--- database/src/routes/index.js
+++ database/src/routes/index.js
@@ -1,27 +1,14 @@
 const { Router } = require("express");
-const store = require("../database")
-const { validateModel } = require("../middleware")
+const controllers = require("../controllers")
+const middleware = require("../middleware")
 
 const router = Router();
 
-router.get("/:model", validateModel, async (req, res)=>{
-    const { model } = req.params
-    const response = await store[model].list()
-    res.status(200).json(response) 
-})
+router.get("/:model", middleware.validateModel, controllers.getModel)
 
-router.get("/:model/:id", validateModel, async (req, res)=>{
-    const { model, id } = req.params;
-    const response = await store[model].get(id);
-    res.status(200).json(response) 
-})
+router.get("/:model/:id", middleware.validateModel, controllers.getModelById)
 
-router.post("/:model", validateModel, async (req, res ) => {
-    const { model } = req.params;
-    const data = req.body; 
-    const response = await store[model].insert(data);
-    res.status(200).json(response)   
-})
+router.post("/:model", middleware.validateModel, controllers.createModel)
 
 module.exports = router;
 
