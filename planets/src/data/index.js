4064418561fec0e6edb89fd33d10acbc5a61c6f2
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-07-11 19:18:31 -0300

    docker-compose

--- planets/src/data/index.js
+++ planets/src/data/index.js
@@ -2,10 +2,10 @@ const axios = require("axios");
 
 module.exports = {
     list: async () => {
-        const response = axios.get("http://database:8004/Planet");
-        return response
+        const response = await axios.get("http://database:8004/Planet");
+        return response.data
     },
-    create: async () => {
+    create: () => {
         throw Error("Hubo un error en la BDD al crear el planeta")
     }
 };
