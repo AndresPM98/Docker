5a0c125fe7a5b70f00304194487907d3788912f5
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-07-12 19:28:20 -0300

    instalacion cors

--- gateway/index.js
+++ gateway/index.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors")
+const {createProxyMiddleware} = require ("http-proxy-middleware")
 
 const app = express();
 app.use(morgan("dev"));
-app.use(express.json());
+
+app.use(express.json())
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
-};
-
-app.use(cors(corsOptions));
+    origin:'http://localhost:5173',
+}
+app.use(cors(corsOptions))
 
 app.use("/characters", createProxyMiddleware({
     target: "http://characters:8001",
     changeOrigin: true,
-}));
+}))
 
 app.use("/films", createProxyMiddleware({
     target: "http://films:8002",
     changeOrigin: true,
-}));
+}))
 
 app.use("/planets", createProxyMiddleware({
     target: "http://planets:8003",
     changeOrigin: true,
-}));
+}))
 
-app.listen(8000, () => {
+app.listen(8000,()=>{
     console.log("Gateway on port 8000");
-});
+})
